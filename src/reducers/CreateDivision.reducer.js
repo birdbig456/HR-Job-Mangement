e0944efca4f7e5a1cd2e08f9f7a9a7a4c667f993
birdbig456 <birdbig456@gmail.com>
2020-09-28 18:22:39 +0700

    WF capacity edit  by bird

--- src/reducers/CreateDivision.reducer.js
+++ src/reducers/CreateDivision.reducer.js
@@ -7,7 +7,7 @@ const NameDivision = [
   export default (state = { NameDivision }, { type, payload }) => {
     switch (type) {
       case CREATEDIVISION_ACTION_CREATE:
-        state.NameDivision.push(payload);
+        state.NameDivision.splice(0,1,payload);
         return state;
   
       default:
