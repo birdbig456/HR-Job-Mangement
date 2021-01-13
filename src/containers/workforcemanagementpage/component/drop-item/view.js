1dadef9686bbc1e66dcb2cfc19e47135eca2d2f3
thanuchk <60010414@kmitl.ac.th>
2021-01-13 20:26:33 +0700

    drag-drop redesigned again

--- src/containers/workforcemanagementpage/component/drop-item/view.js
+++ src/containers/workforcemanagementpage/component/drop-item/view.js
@@ -3,7 +3,7 @@ import "./styles.css";
 
 export default forwardRef(({ children, heading }, ref) => {
   return (
-    <div class="container" ref={ref}>
+    <div class="container" ref={ref} style={{display:"flex",justifyContent:"center",alignItems:"start"}}>
       <div>{children}</div>
     </div>
   );
