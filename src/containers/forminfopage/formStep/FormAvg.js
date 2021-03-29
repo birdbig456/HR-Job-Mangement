fe85989f3f5e6da24f23ecc589c16cbc550216c3
thanuchk <60010414@kmitl.ac.th>
2021-03-30 01:38:48 +0700

    formavg edited

--- src/containers/forminfopage/formStep/FormAvg.js
+++ src/containers/forminfopage/formStep/FormAvg.js
@@ -1,250 +1,406 @@
 import React, { useState } from "react";
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import InputBase from '@material-ui/core/InputBase';
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import InputBase from "@material-ui/core/InputBase";
 import Grid from "@material-ui/core/Grid";
-const BootstrapInput = withStyles((theme) => ({
-    root: {
-      'label + &': {
-        marginTop: theme.spacing(3),
-      },
-    },
-    input: {
-      borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      // Use the system font instead of the default Roboto font.
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
-    },
-  }))(InputBase);
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
+
   margin: {
     margin: theme.spacing(1),
   },
-  withoutLabel: {
-    marginTop: theme.spacing(3),
+  MHbox: {
+    margin: theme.spacing(1),
+    minWidth:480
   },
   textField: {
-    width: '25ch',
+    width: "25ch",
   },
-  
+  textLabel:{
+    margin: theme.spacing(1),
+    minWidth:220
+  },
 }));
 
 export default function FormAvg() {
   const classes = useStyles();
   const [reMark, setReMark] = React.useState({
-    ReUnexpected: '',
-    ReMeeting: '',
-    ReVoyage: '',
-    RePrepare: '',
-    ReTraing: '',
-    ReOther: '',
+    ReUnexpected: "",
+    ReMeeting: "",
+    ReVoyage: "",
+    RePrepare: "",
+    ReTraining: "",
+    ReOther: "",
   });
-
 
   const handleChange = (prop) => (event) => {
     setReMark({ ...reMark, [prop]: event.target.value });
   };
-  
-  const [manHour, setManHour] = React.useState({
-    Unexpected: '',
-    Meeting: '',
-    Voyage: '',
-    Prepare: '',
-    Traing: '',
-    Other: '',
-  });
 
+  const [manHour, setManHour] = React.useState({
+    Unexpected: "",
+    Meeting: "",
+    Voyage: "",
+    Prepare: "",
+    Training: "",
+    Other: "",
+  });
 
   const handleChange2 = (prop1) => (event) => {
     setManHour({ ...manHour, [prop1]: event.target.value });
   };
 
-console.log(reMark,manHour)
+  console.log(reMark, manHour);
   return (
-    <div className={classes.root}>
-        <Grid container>
-            <Grid item xs={12} sm={5}>
-                Average Unexpected Task
-            </Grid>
-            <Grid item xs={6} sm={5}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">ManHour/Month</InputLabel>
+    <div >
+      {/* <Grid container>
+        <Grid item xs={12} sm={5}>
+          Average Unexpected Task
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">
+              ManHour/Month
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={manHour.Unexpected}
+              onChange={handleChange2("Unexpected")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={reMark.ReUnexpected}
+              onChange={handleChange("ReUnexpected")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          Average Meeting Time
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="-adornment-amount">ManHour/Month</InputLabel>
+            <OutlinedInput
+              id=""
+              value={manHour.Meeting}
+              onChange={handleChange2("Meeting")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={reMark.ReMeeting}
+              onChange={handleChange("ReMeeting")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          Average Voyage Time
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">
+              ManHour/Month
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={manHour.Voyage}
+              onChange={handleChange2("Voyage")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={reMark.ReVoyage}
+              onChange={handleChange("ReVoyage")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          Average Prepare Date Time
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">
+              ManHour/Month
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={manHour.Prepare}
+              onChange={handleChange2("Prepare")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={reMark.RePrepare}
+              onChange={handleChange("RePrepare")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          Average Training Time
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">
+              ManHour/Month
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={manHour.Training}
+              onChange={handleChange2("Training")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={reMark.ReTraining}
+              onChange={handleChange("ReTraining")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          Other Task Time
+        </Grid>
+        <Grid item xs={6} sm={5}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">
+              ManHour/Month
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={manHour.Other}
+              onChange={handleChange2("Other")}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sm={2}>
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={reMark.ReOther}
+              onChange={handleChange("ReOther")}
+            />
+          </FormControl>
+        </Grid>
+      </Grid> */}
+
+     
+
+      <Grid container style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className={classes.textLabel}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          Average Unexpected Task :
+        </div>
+        <FormControl className={classes.MHbox} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-amount">
+            ManHour / Month
+          </InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="ManHour / Month"
             id="outlined-adornment-amount"
             value={manHour.Unexpected}
-            onChange={handleChange2('Unexpected')}
-            
+            onChange={handleChange2("Unexpected")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={2}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+
+        <FormControl className={classes.margin} variant="outlined">
+          <InputLabel>Remark</InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="Remark"
             id="outlined-adornment-amount"
             value={reMark.ReUnexpected}
-            onChange={handleChange('ReUnexpected')}
-            
+            onChange={handleChange("ReUnexpected")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={5}>
-                Average Meeting Time
-            </Grid>
-            <Grid item xs={6} sm={5}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="-adornment-amount">ManHour/Month</InputLabel>
+      </Grid>
+
+      <Grid container style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className={classes.textLabel}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          Average Meeting Time :
+        </div>
+        <FormControl className={classes.MHbox} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-amount">
+            ManHour / Month
+          </InputLabel>
           <OutlinedInput
-            id=""
+            variant="outlined"
+            label="ManHour / Month"
+            id="outlined-adornment-amount"
             value={manHour.Meeting}
-            onChange={handleChange2('Meeting')}
-            
+            onChange={handleChange2("Meeting")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={2}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+
+        <FormControl className={classes.margin} variant="outlined">
+          <InputLabel>Remark</InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="Remark"
             id="outlined-adornment-amount"
             value={reMark.ReMeeting}
-            onChange={handleChange('ReMeeting')}
-            
+            onChange={handleChange("ReMeeting")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={5}>
-                Average Voyage Time
-            </Grid>
-            <Grid item xs={6} sm={5}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">ManHour/Month</InputLabel>
+      </Grid>
+
+      <Grid container style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className={classes.textLabel}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          Average Voyage Time :
+        </div>
+        <FormControl className={classes.MHbox} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-amount">
+            ManHour / Month
+          </InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="ManHour / Month"
             id="outlined-adornment-amount"
             value={manHour.Voyage}
-            onChange={handleChange2('Voyage')}
-            
+            onChange={handleChange2("Voyage")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={2}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+
+        <FormControl className={classes.margin} variant="outlined">
+          <InputLabel>Remark</InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="Remark"
             id="outlined-adornment-amount"
             value={reMark.ReVoyage}
-            onChange={handleChange('ReVoyage')}
-            
+            onChange={handleChange("ReVoyage")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={5}>
-                Average Prepare Date Time
-            </Grid>
-            <Grid item xs={6} sm={5}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">ManHour/Month</InputLabel>
+      </Grid>
+
+      <Grid container style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className={classes.textLabel}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          Average Prepare Date Time :
+        </div>
+        <FormControl className={classes.MHbox} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-amount">
+            ManHour / Month
+          </InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="ManHour / Month"
             id="outlined-adornment-amount"
             value={manHour.Prepare}
-            onChange={handleChange2('Prepare')}
-            
+            onChange={handleChange2("Prepare")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={2}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+
+        <FormControl className={classes.margin} variant="outlined">
+          <InputLabel>Remark</InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="Remark"
             id="outlined-adornment-amount"
             value={reMark.RePrepare}
-            onChange={handleChange('RePrepare')}
-            
+            onChange={handleChange("RePrepare")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={5}>
-                Average Traing Time
-            </Grid>
-            <Grid item xs={6} sm={5}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">ManHour/Month</InputLabel>
+      </Grid>
+
+      <Grid container style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className={classes.textLabel}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          Average Training Time :
+        </div>
+        <FormControl className={classes.MHbox} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-amount">
+            ManHour / Month
+          </InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="ManHour / Month"
             id="outlined-adornment-amount"
-            value={manHour.Traing}
-            onChange={handleChange2('Traing')}
-            
+            value={manHour.Training}
+            onChange={handleChange2("Training")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={2}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+
+        <FormControl className={classes.margin} variant="outlined">
+          <InputLabel>Remark</InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="Remark"
             id="outlined-adornment-amount"
-            value={reMark.ReTraing}
-            onChange={handleChange('ReTraing')}
-            
+            value={reMark.ReTraining}
+            onChange={handleChange("ReTraining")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={5}>
-                Other Task Time 
-            </Grid>
-            <Grid item xs={6} sm={5}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">ManHour/Month</InputLabel>
+      </Grid>
+
+      <Grid container style={{ display: "flex", justifyContent: "center" }}>
+        <div
+          className={classes.textLabel}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          Other Task Time :
+        </div>
+        <FormControl className={classes.MHbox} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-amount">
+            ManHour / Month
+          </InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="ManHour / Month"
             id="outlined-adornment-amount"
             value={manHour.Other}
-            onChange={handleChange2('Other')}
-            
+            onChange={handleChange2("Other")}
           />
         </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={2}>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-amount">Remark</InputLabel>
+
+        <FormControl className={classes.margin} variant="outlined">
+          <InputLabel>Remark</InputLabel>
           <OutlinedInput
+            variant="outlined"
+            label="Remark"
             id="outlined-adornment-amount"
             value={reMark.ReOther}
-            onChange={handleChange('ReOther')}
-            
+            onChange={handleChange("ReOther")}
           />
         </FormControl>
-            </Grid>
-
-        </Grid>        
+      </Grid>
     </div>
   );
 }
